457e7098d1cb687b36f82a049827000f82884c53
sergidl <sergidiazlopez3@gmail.com>
2022-04-28 17:37:15 +0200

    colors

---
+++ cat.institutmvm.application/src/cat/institutmvm/application/entities/Colour.java
@@ -0,0 +1,65 @@
+package cat.institutmvm.application.entities;
+
+public class Colour {
+	private String name;
+	private int red, green, blue;
+	public static final String NO_NAME = "undefined";
+	private static int counter = 0;
+	public static final int MIN_VALUE = 0;
+	public static final int MAX_VALUE = 255;
+
+	public Colour(String name, int red, int green, int blue) {
+		this.setName(name);
+		this.setRed(red);
+		this.setGreen(green);
+		this.setBlue(blue);
+	}
+
+	// public Colour(int red, int green, int blue) {
+	// 	this(NO_NAME, red, green, blue);
+	// }
+	// public Colour() {
+	// 	this(NO_NAME, MAX_VALUE, MAX_VALUE, MAX_VALUE);
+	// }
+	// public Colour(String name) {
+	// 	this(name, 2, 4, 6);
+	// }
+	
+
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRed() {
+		return this.red;
+	}
+
+	public void setRed(int red) {
+		this.red = red;
+	}
+
+	public int getGreen() {
+		return this.green;
+	}
+
+	public void setGreen(int green) {
+		this.green = green;
+	}
+
+	public int getBlue() {
+		return this.blue;
+	}
+
+	public void setBlue(int blue) {
+		this.blue = blue;
+	}
+
+
+	
+}
--- com.institutmvm.controller/src/com/institutmvm/controller/ui/App.java
+++ com.institutmvm.controller/src/com/institutmvm/controller/ui/App.java
@@ -1,14 +1,21 @@
 package com.institutmvm.controller.ui;
 
-import cat.institutmvm.application.entities.Persona;
+import cat.institutmvm.application.entities.Colour;
 import cat.institutmvm.application.utils.MyMath;
 public class App {
 	public static void main(String[] args) {
-		Persona p = new Persona("Jordi");
-		System.out.println(p.getNom());
 		System.out.println(MyMath.isEven(2));
 		System.out.println(MyMath.min(1,2,3,4,5,9,8,7,6));
 		System.out.println(MyMath.max(1,2,3,4,5,9,8,7,6));
+		
+		//random colour
+		Colour c = new Colour('asdasd',2,4,6);
+		System.out.println(c.getName());
+		System.out.println(c.getRed());
+		System.out.println(c.getGreen());
+		System.out.println(c.getBlue());
+
+		
 	}
 	
 }
